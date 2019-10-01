4013f19f0d73aab57484887909dc4242ef8355a3
EWCMF <TGHpublic@hotmail.com>
2019-10-01 17:28:24 +0200

    fast

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sten saks papir.");
+
+        // Test enum.
+        Haand spiller1 = Haand.PAPIR;
+        Haand spiller2 = Haand.SAKS;
+
+        // Her starter spillet
+        SSP runde1 = new SSP();
+        System.out.println(runde1.slaa(spiller1, spiller2));
+
+        // Computer generer en hånd.
+        Haand computersHaand;
+        int haandTal = (int) Math.random() * 3 + 1;
+        Haand.values(0);
+        // Indlæse spillerens hånd.
+        // Sammenlign med slaa() metoden.
+        // Udråbe en vinder.
+    }
 }
--- src/SSP.java
+++ src/SSP.java
@@ -1,2 +1,63 @@
 public class SSP {
+
+    /** Hvis den første hånd vinder, returneres 1
+     * Hvis den første hånd vinder, returneres 2
+     * Hvis uafgjort, returneres 0
+     * Hvis fejl, returneres -1 */
+    int slaa(Haand a, Haand b) {
+
+        int resultat = -1;
+
+        // første hånd sten.
+        if (a == Haand.STEN) {
+            // Anden hånd b kan være sten.
+            if (b == Haand.STEN) {
+                resultat = 0;
+            }
+            // Anden hånd b kan være saks.
+            if (b == Haand.SAKS) {
+                resultat = 1;
+            }
+            // Anden hånd b kan være papir.
+            if (b == Haand.PAPIR) {
+                resultat = 2;
+            }
+        }
+
+        // første hånd saks.
+        if (a == Haand.SAKS) {
+            // Anden hånd b kan være sten.
+            if (b == Haand.STEN) {
+                resultat = 2;
+            }
+            // Anden hånd b kan være saks.
+            if (b == Haand.SAKS) {
+                resultat = 0;
+            }
+            // Anden hånd b kan være papir.
+            if (b == Haand.PAPIR) {
+                resultat = 1;
+            }
+        }
+
+        // første hånd papir.
+        if (a == Haand.PAPIR) {
+            // Anden hånd b kan være sten.
+            if (b == Haand.STEN) {
+                resultat = 1;
+            }
+            // Anden hånd b kan være saks.
+            if (b == Haand.SAKS) {
+                resultat = 2;
+            }
+            // Anden hånd b kan være papir.
+            if (b == Haand.PAPIR) {
+                resultat = 0;
+            }
+        }
+
+        return resultat;
+    }
+
+    // TODO: flereKampe(){}
 }

c0908a89842421e6008218f940cb3be4c09c83f2
Tommy <TGHpublic@hotmail.com>
2019-10-02 10:41:34 +0200

    Taget kode fra András

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Sten saks papir.");
@@ -10,12 +12,29 @@ public class Main {
         SSP runde1 = new SSP();
         System.out.println(runde1.slaa(spiller1, spiller2));
 
-        // Computer generer en hånd.
-        Haand computersHaand;
-        int haandTal = (int) Math.random() * 3 + 1;
-        Haand.values(0);
-        // Indlæse spillerens hånd.
-        // Sammenlign med slaa() metoden.
-        // Udråbe en vinder.
+        // Computer genererer en hånd
+        Haand computerensHaand = null;
+        int haandInt =(int) (Math.random()*3+1);
+        // Korte nørklet kode  computersHaand = Haand.values()[haandInt];
+        if (haandInt == 1) { computerensHaand = Haand.STEN; }
+        if (haandInt == 2) { computerensHaand = Haand.SAKS; }
+        if (haandInt == 3) { computerensHaand = Haand.PAPIR; }
+        System.out.println("Computer slår " + computerensHaand);
+
+        // Indlæse spillerns hånd
+        Scanner scanner = new Scanner(System.in);
+        Haand spillerensHaand = null;
+        String spillerenString = scanner.next();
+        if (spillerenString.equals("sten")) { spillerensHaand = Haand.STEN; }
+        if (spillerenString.equals("saks")) { spillerensHaand = Haand.SAKS; }
+        if (spillerenString.equals("papir")) { spillerensHaand = Haand.PAPIR; }
+        System.out.println("Spiller slår " + spillerensHaand);
+
+        // Sammenligne med slaa() metoden
+        int resultat = runde1.slaa(computerensHaand, spillerensHaand);
+
+        // Udråbe en vinder
+        System.out.println("Resultatet er " + resultat);
+        // TODO vis resultatet mere meningsfuldt
     }
 }

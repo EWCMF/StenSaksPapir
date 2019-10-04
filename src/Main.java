5f7fcab50965ca2a3d6a196dd246f3d53863613e
Tommy <TGHpublic@hotmail.com>
2019-10-04 14:09:45 +0200

    Scoreliste

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,9 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Sten saks papir.");
 
-        // Test enum.
-        Haand spiller1 = Haand.PAPIR;
-        Haand spiller2 = Haand.SAKS;
+        SSP.startSpillet();
 
-        // Her starter spillet
-        SSP runde1 = new SSP();
-        System.out.println(runde1.slaa(spiller1, spiller2));
-
-
-        // Indlæse spillerns hånd
-        System.out.println("Vælg en hånd.");
-        Scanner scanner = new Scanner(System.in);
-        Haand spillerensHaand = null;
-        String spillerenString = scanner.next();
-        if (spillerenString.equals("sten")) { spillerensHaand = Haand.STEN; }
-        if (spillerenString.equals("saks")) { spillerensHaand = Haand.SAKS; }
-        if (spillerenString.equals("papir")) { spillerensHaand = Haand.PAPIR; }
-        System.out.println("Spiller slår " + spillerensHaand);
-
-        // Computer genererer en hånd
-        Haand computerensHaand = null;
-        int haandInt =(int) (Math.random()*3+1);
-        // Korte nørklet kode  computersHaand = Haand.values()[haandInt];
-        if (haandInt == 1) { computerensHaand = Haand.STEN; }
-        if (haandInt == 2) { computerensHaand = Haand.SAKS; }
-        if (haandInt == 3) { computerensHaand = Haand.PAPIR; }
-        System.out.println("Computer slår " + computerensHaand);
-
-
-        // Sammenligne med slaa() metoden
-        int resultat = runde1.slaa(computerensHaand, spillerensHaand);
-
-        // Udråbe en vinder
-        System.out.println("Resultatet er " + resultat);
-        // TODO vis resultatet mere meningsfuldt
     }
+
 }
--- src/SSP.java
+++ src/SSP.java
@@ -1,4 +1,79 @@
+import java.util.Scanner;
+
 public class SSP {
+
+    public static void startSpillet() {
+        SSP runde1 = new SSP() ;
+
+        int win = 0;
+        int lose = 0;
+        int draw = 0;
+
+        int count = 1;
+        while (count >= 0) {
+
+            // Indlæse spillerns hånd
+            System.out.println("Runde " + count + " - Hvad slår du? (sten, saks, papir)");
+            Scanner scanner = new Scanner(System.in);
+            Haand spillerensHaand = null;
+            String spillerenString = scanner.next();
+            if (spillerenString.equalsIgnoreCase("sten")) {
+                spillerensHaand = Haand.STEN;
+            }
+            if (spillerenString.equalsIgnoreCase("saks")) {
+                spillerensHaand = Haand.SAKS;
+            }
+            if (spillerenString.equalsIgnoreCase("papir")) {
+                spillerensHaand = Haand.PAPIR;
+            }
+            if (spillerenString.equalsIgnoreCase("afslut")) {
+                System.out.println("Du vandt: " + win + " gang(e)");
+                System.out.println("Du tabte: " + lose + " gang(e)");
+                System.out.println("Det blev uafgjort: " + draw + " gang(e)");
+                break;
+            }
+            System.out.println("Spiller slår " + spillerensHaand);
+
+            // Computer genererer en hånd
+            Haand computerensHaand = null;
+            int haandInt = (int) (Math.random() * 3 + 1);
+            // Korte nørklet kode  computersHaand = Haand.values()[haandInt];
+            if (haandInt == 1) {
+                computerensHaand = Haand.STEN;
+            }
+            if (haandInt == 2) {
+                computerensHaand = Haand.SAKS;
+            }
+            if (haandInt == 3) {
+                computerensHaand = Haand.PAPIR;
+            }
+            System.out.println("Computer slår " + computerensHaand);
+
+
+            // Sammenligne med slaa() metoden
+            int resultat = runde1.slaa(spillerensHaand, computerensHaand);
+
+            // Udråbe en vinder
+            if (resultat == 1) {
+                System.out.println("Du har vundet");
+                count++;
+                win++;
+            }
+            if (resultat == 2) {
+                System.out.println("Du har tabt");
+                count++;
+                lose++;
+            }
+            if (resultat == 0) {
+                System.out.println("Det er uafgjort");
+                count++;
+                draw++;
+            }
+            if (resultat == -1) {
+                System.out.println("Fejl");
+            }
+        }
+    }
 
     /** Hvis den første hånd vinder, returneres 1
      * Hvis den første hånd vinder, returneres 2
@@ -58,6 +133,4 @@ public class SSP {
 
         return resultat;
     }
-
-    // TODO: flereKampe(){}
 }

300df55779924078424e1f7504ce972db762f2f1
Tommy <TGHpublic@hotmail.com>
2019-10-04 14:50:49 +0200

    Resten af test

--- src/SSPTest.java
+++ src/SSPTest.java
@@ -2,7 +2,7 @@ import junit.framework.TestCase;
 
 public class SSPTest extends TestCase {
 
-    public void testSlaa() {
+    public void testSlaaSten() {
         Haand input1 = Haand.STEN;
         Haand input2 = Haand.STEN;
 
@@ -17,6 +17,46 @@ public class SSPTest extends TestCase {
 
         input1 = Haand.STEN;
         input2 = Haand.PAPIR;
+
+        output = SSP.slaa(input1, input2);
+        assertEquals(2, output);
+    }
+
+    public void testSlaaSaks() {
+        Haand input1 = Haand.SAKS;
+        Haand input2 = Haand.SAKS;
+
+        int output = SSP.slaa(input1, input2);
+        assertEquals(0, output);
+
+        input1 = Haand.SAKS;
+        input2 = Haand.PAPIR;
+
+        output = SSP.slaa(input1, input2);
+        assertEquals(1, output);
+
+        input1 = Haand.SAKS;
+        input2 = Haand.STEN;
+
+        output = SSP.slaa(input1, input2);
+        assertEquals(2, output);
+    }
+
+    public void testSlaaPapir() {
+        Haand input1 = Haand.PAPIR;
+        Haand input2 = Haand.PAPIR;
+
+        int output = SSP.slaa(input1, input2);
+        assertEquals(0, output);
+
+        input1 = Haand.PAPIR;
+        input2 = Haand.STEN;
+
+        output = SSP.slaa(input1, input2);
+        assertEquals(1, output);
+
+        input1 = Haand.PAPIR;
+        input2 = Haand.SAKS;
 
         output = SSP.slaa(input1, input2);
         assertEquals(2, output);
